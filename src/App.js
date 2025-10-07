c582234762702394c948cf00c6b6614d397e1029
Francisco Torres <franc.torresb@duocuc.cl>
2025-10-06 21:02:41 -0300

    mi segundo commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row mt-3">
+          <div className="col-12">
+            <h2>Pagina Para calculo de remuneracion</h2>
+          </div>
+        </div>
+
+        <div className="row mt-3">
+          <div className="col-6 d-flex flex-column align-items-center">
+            <h3>datos sueldo bruto</h3>
+            <div className="mb-3 mt-3 text-start w-75">
+              <label htmlFor="sueldoBruto" className="form-label">Ingrese sueldo bruto</label>
+              <input id="sueldoBruto" type="number" className="form-control" placeholder="Ingrese sueldo bruto" />
+            </div>
+          </div>
+
+          <div className="col-6 d-flex flex-column align-items-center">
+            <h3>resultados</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
